83de6d0ed1f4399f44770e733b169958b9d8360e
spiderbad123 <141422568+spiderbad123@users.noreply.github.com>
2024-06-27 23:26:28 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 function handleSubmit(){
-   
+//  lấy giá trị của input email và chuyển sang in thường
     const emailValue = document.getElementById("email").value.toLocaleLowerCase()
+
+    //lấy element của thẻ p hiển thị lỗi
+    const errorEmail = document.getElementById('error-email')
+    //điều kiện để là 1 email
     const checkMail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    //Dùng match để kiểm tra email vừa nhập
     console.log("check mail",emailValue)
-    emailValue.match(checkMail)
+    const check = emailValue.match(checkMail)
+    
+    // lấy phần chứa thông tin user
+    const sectionContent = document.querySelector('#info .section-content')
+    console.log('check section' , sectionContent)
+
+    //  lấy element để kiểm soát submit
+    const submitControl = document.querySelector('.submit-email')
+    if(check){
+        sectionContent.style.display = 'block'
+        submitControl.style.display = 'none'
+        errorEmail.innerHTML=''
+    }else{
+        errorEmail.innerHTML='Vui lòng nhập đúng định dạng email'
+    }
+
+}
+function handleOnMouseOver(element){
+   
+    const viewMore = element.querySelector('.view-more')
+    viewMore.style.display = 'inline-block'
+}
+
+function handleOnMouseOut(element){
+   const viewMore = element.querySelector('.view-more')
+    viewMore.style.display = 'none'
 }
